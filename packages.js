4e3ec7efdbc6875c3a3d00b43fceeb3dc3bc1c81
Ktech <54085500+KingCrypto285@users.noreply.github.com>
2025-06-30 23:29:24 +1200

    adding in new gifs
    ..

--- packages.js
+++ packages.js
@@ -5,17 +5,28 @@ const packages = [
     version: "0.1.2",
     link: "https://www.npmjs.com/package/react-native-styled-flatlists",
     img: [
-      { title: 'Pulse Enter List', src: 'gifs/PulseEnterList.gif' }
+      { title: 'Pulse Enter List', src: 'gifs/PulseInTiming.gif' },
+      { title: 'Slide In List', src: 'gifs/horizontalslideinlist.gif' },
+      { title: 'Slide In List', src: 'gifs/verticalslideinlist .gif' },
     ]
   },
   {
     name: "Styled Toggle Switches",
     description: "React Native toggle switches with style",
-    version: "0.1.1",
+    version: "0.1.2",
     link: "https://www.npmjs.com/package/react-native-styled-toggle-switches",
     img: [
       { title: 'Basic Slide Switch', src: 'gifs/slideswitch1.gif' },
       { title: 'Slide Switch with Text', src: 'gifs/slideswitchwithtext.gif' }
+    ]
+  },
+  {
+    name: "React Native Styled Badges",
+    description: "React Native toggle switches with style",
+    version: "0.1.0",
+    link: "https://www.npmjs.com/package/react-native-styled-badges",
+    img: [
+      { title: 'Basic Badge', src: 'img/badgeStack.png' }, 
     ]
   }
 ];
